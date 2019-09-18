3f40d12114898a194e4d9e772a0e93938ce07519
renaldyhidayatt <renaldyhidayatt@gmail.com>
2019-09-18 23:35:16 +0700

    Pusher singlephaser.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(__dirname + '/public/'));
+app.get('/', (req, res) => res.sendFile('index.html'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
